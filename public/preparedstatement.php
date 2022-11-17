8757f668f95a7075f0af840cd843bc48964dfb38
Pablo Train <pablotrain25@gmail.com>
2022-11-17 15:49:02 +0000

    rama mvc03

---
+++ public/preparedstatement.php
@@ -0,0 +1,49 @@
+<?php
+
+    $dsn = "mysql:dbname=demo;host=db";
+    $usuario = "dbuser";
+    $password = "secret";
+
+    /*
+        1. Preparar la consulta -> prepare()
+        2. Vincular los datos -> bindParam/bindValue
+        3. Ejecutar las sentencias -> execute() (query, exec);
+    */
+
+    try{
+        $db = new PDO($dsn, $usuario, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //Preparamos la consulta por nombre
+        //$sentencia = $db->prepare("INSERT INTO credenciales (nombreusu, password) VALUES (:nombre, :clave)");
+        
+        //Preparamos la consulta por posicion
+        $sentencia = $db->prepare("INSERT INTO credenciales (nombreusu, password) VALUES (:nombre, :clave)");
+        
+        
+        $nombre = "Pedro";
+        $clave = "555";
+
+        //Vincular datos con bindParam, se puede sobreescribir
+        //$sentencia->bindParam(":nombre",$nombre);
+        //$sentencia->bindParam(":clave",$clave1);
+
+        //Vincular datos con bindValue, no se sobreescribe
+        //$sentencia->bindValue(1,$nombre);
+        //$sentencia->bindValue(2,$clave1);
+
+        //$nombre = "Pedro";
+        //$clave1 = "555";
+
+
+        //Ejecutamos la sentencia
+        //$sentencia->execute();
+
+        //Execute con array
+        $sentencia->execute(array(':nombre' => $nombre, ':clave' => $clave));
+
+        echo "<h2>Éxito</h2>";
+
+    }catch(PDOException $e){
+        echo "Error producido al conectar: " . $e->getMessage();
+    }
